3248f22b5bddde39ccec46e1124d9dbfb4d0319c
Tsukinatsune - Administrator of artoriasphere <ChaluckyChannel@gmail.com>
2025-10-12 22:50:07 +0700

    Update lazysrcob4.js

--- lazysrcob4.js
+++ lazysrcob4.js
@@ -1,22 +1,24 @@
-window.addEventListener('load', () => {
-  const images = document.querySelectorAll('img[lazysrc]');
-  
-  const observerOptions = {
-    root: null,
-    rootMargin: '0px 0px 100px 0px',
-    threshold: 0.1
-  };
+const startLazyLoading = () => {
+      const images = document.querySelectorAll('img[lazysrc]');
+      
+      const observerOptions = {
+        root: null,
+        rootMargin: '0px 0px 100px 0px',
+        threshold: 0.1
+      };
 
-  const imageObserver = new IntersectionObserver((entries, observer) => {
-    entries.forEach(entry => {
-      if (entry.isIntersecting) {
-        const img = entry.target;
-        img.src = img.getAttribute('lazysrc');
-        img.removeAttribute('lazysrc');
-        observer.unobserve(img);
-      }
-    });
-  }, observerOptions);
+      const imageObserver = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+          if (entry.isIntersecting) {
+            const img = entry.target;
+            img.src = img.getAttribute('lazysrc');
+            img.removeAttribute('lazysrc');
+            observer.unobserve(img);
+          }
+        });
+      }, observerOptions);
 
-  images.forEach(image => imageObserver.observe(image));
-});
+      images.forEach(image => imageObserver.observe(image));
+      window.removeEventListener('scroll', startLazyLoading);
+    };
+window.addEventListener('scroll', startLazyLoading);
