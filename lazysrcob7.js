358b886b8d4f486649fe02c0aa6e64ee03e1b7f7
Tsukinatsune - Administrator of artoriasphere <ChaluckyChannel@gmail.com>
2025-10-12 23:02:17 +0700

    Update and rename lazysrcob6.js to lazysrcob7.js

--- lazysrcob7.js
+++ lazysrcob7.js
@@ -1,6 +1,8 @@
-setTimeout(() => {
+function lazyLoad() {
   const images = document.querySelectorAll('img[lazysrc]');
-      
+  
+  if (images.length === 0) return; // Exit if no images to process
+  
   const observerOptions = {
     root: null,
     rootMargin: '0px 0px 100px 0px',
@@ -14,9 +16,14 @@ setTimeout(() => {
         img.src = img.getAttribute('lazysrc');
         img.removeAttribute('lazysrc');
         observer.unobserve(img);
+        
+        // Check if all images are processed
+        if (document.querySelectorAll('img[lazysrc]').length === 0) {
+          observer.disconnect(); // Clear the observer
+        }
       }
     });
   }, observerOptions);
 
   images.forEach(image => imageObserver.observe(image));
-}, 5000);
+}
